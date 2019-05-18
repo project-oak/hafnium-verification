d6db478cd3521dca1c97967269ef5bf0a6bc07e8
Jeehoon Kang <jeehoon.kang@kaist.ac.kr>
2019-05-19 01:32:42 +0900

    Oxidize src/abort.c

---
+++ hfo2/src/abort.rs
@@ -0,0 +1,14 @@
+/// Causes execution to halt and prevent progress of the current and less privileged software
+/// components. This should be triggered when a non-recoverable event is identified which leaves the
+/// system in an inconsistent state.
+///
+/// TODO: Should this also reset the system?
+#[cfg(not(feature = "test"))]
+#[no_mangle]
+pub unsafe extern "C" fn abort() -> ! {
+	  // TODO: Block all CPUs.
+    loop {
+		    // Prevent loop being optimized away.
+        asm!("nop");
+    }
+}
