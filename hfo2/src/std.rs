0b6b05898c688e2aa2498b4b2597127d283ca6b9
Jeehoon Kang <jeehoon.kang@kaist.ac.kr>
2019-05-18 23:03:48 +0900

    Oxidize src/std.c

--- hfo2/src/std.rs
+++ hfo2/src/std.rs
@@ -1,16 +1,71 @@
+use core::mem;
+use core::ptr;
+
 use crate::types::*;
 
-extern "C" {
-    fn memset(s: *const c_void, c: c_int, n: size_t) -> *const c_void;
-}
-
 #[no_mangle]
-pub extern "C" fn memset_s(dest: *const c_void, destsz: size_t, ch: c_int, count: size_t) {
+pub unsafe extern "C" fn memset_s(dest: *const c_void, destsz: size_t, ch: c_int, count: size_t) {
     if dest.is_null() || destsz > RSIZE_MAX || count > RSIZE_MAX || count > destsz {
         panic!("memset_s failure");
     }
 
-    unsafe {
-        memset(dest, ch, count);
+    ptr::write_bytes(dest as *mut u8, ch as u8, count);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn memcpy_s(
+    dest: *mut c_void,
+    destsz: size_t,
+    src: *const c_void,
+    count: size_t,
+) {
+    let d = dest as usize;
+    let s = src as usize;
+
+    if dest.is_null() || src.is_null()
+        || destsz > RSIZE_MAX || count > RSIZE_MAX
+        || count > destsz
+    // Destination overlaps the end of source.
+        || (d > s && d < s + count)
+    // Source overlaps the end of destination.
+        || (s > d && s < d + destsz)
+    {
+        panic!("memcpy_s failure");
     }
+
+    // TODO: consider wrapping?
+
+    ptr::copy(src as *const u8, dest as *mut u8, count);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn memmove_s(
+    dest: *mut c_void,
+    destsz: size_t,
+    src: *const c_void,
+    count: size_t,
+) {
+    if dest.is_null() || src.is_null() || destsz > RSIZE_MAX || count > RSIZE_MAX || count > destsz
+    {
+        panic!("memmove_s failure");
+    }
+
+    // FIXME(@jeehoonkang): `ptr::copy_nonoverlapping()` is more appropriate here, but using it
+    // makes Hafnium crash at boot.
+    ptr::copy(src as *const u8, dest as *mut u8, count);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn strnlen_s(str: *const c_char, mut strsz: size_t) -> size_t {
+    if str.is_null() {
+        return 0;
+    }
+
+    let mut p = str;
+    while strsz > 0 && *p != 0 {
+        strsz -= 1;
+        p = p.add(1);
+    }
+
+    ((p as usize) - (str as usize)) / mem::size_of::<c_char>()
 }
--- hfo2/src/types.rs
+++ hfo2/src/types.rs
@@ -4,6 +4,7 @@ use core;
 
 pub type c_void = core::ffi::c_void;
 pub type c_int = i32;
+pub type c_char = u8;
 pub type size_t = usize;
 pub type rsize_t = usize;
 
