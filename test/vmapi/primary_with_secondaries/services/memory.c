f7632975f200219f84cdd45e26dc0749c4cf165c
Marc Bonnici <marc.bonnici@arm.com>
2019-06-10 09:55:03 +0100

    SPCI Lend and Relinquish: Add initial tests to exercise functionality
    Add tests to verify core functionality of `spci_memory_lend`,
    `spci_memory_relinquish` and their interactions with
    `spci_memory_donate`.
    
    Change-Id: I440d0a385d27f0aad370eb555e2d20e3be726328

--- test/vmapi/primary_with_secondaries/services/memory.c
+++ test/vmapi/primary_with_secondaries/services/memory.c
@@ -147,7 +147,7 @@ TEST_SERVICE(give_memory_and_fault)
 			  hf_vm_get_id());
 	EXPECT_EQ(spci_msg_send(0), 0);
 
-	/* Try using the memory that isn't valid unless it's been returned.  */
+	/* Try using the memory that isn't valid unless it's been returned. */
 	page[16] = 123;
 }
 
@@ -171,7 +171,7 @@ TEST_SERVICE(lend_memory_and_fault)
 			  hf_vm_get_id());
 	EXPECT_EQ(spci_msg_send(0), 0);
 
-	/* Try using the memory that isn't valid unless it's been returned.  */
+	/* Try using the memory that isn't valid unless it's been returned. */
 	page[633] = 180;
 }
 
@@ -280,21 +280,21 @@ TEST_SERVICE(spci_donate_twice)
 	struct spci_message *send_buf = SERVICE_SEND_BUFFER();
 	struct spci_memory_region *memory_region =
 		spci_get_donated_memory_region(recv_buf);
+	struct spci_memory_region_constituent constituent =
+		memory_region->constituents[0];
 	hf_mailbox_clear();
 
 	/* Yield to allow attempt to re donate from primary. */
 	spci_yield();
 
 	/* Give the memory back and notify the sender. */
-	spci_memory_donate(send_buf, HF_PRIMARY_VM_ID, recv_buf->target_vm_id,
-			   memory_region->constituents, memory_region->count,
-			   0);
+	spci_memory_donate(send_buf, HF_PRIMARY_VM_ID, SERVICE_VM0,
+			   &constituent, memory_region->count, 0);
 	EXPECT_EQ(spci_msg_send(0), SPCI_SUCCESS);
 
 	/* Attempt to donate the memory to another VM. */
 	spci_memory_donate(send_buf, SERVICE_VM1, recv_buf->target_vm_id,
-			   memory_region->constituents, memory_region->count,
-			   0);
+			   &constituent, memory_region->count, 0);
 	EXPECT_EQ(spci_msg_send(0), SPCI_INVALID_PARAMETERS);
 
 	spci_yield();
@@ -350,4 +350,269 @@ TEST_SERVICE(spci_donate_invalid_source)
 			   0);
 	EXPECT_EQ(spci_msg_send(0), SPCI_INVALID_PARAMETERS);
 	spci_yield();
+}
+
+TEST_SERVICE(spci_memory_lend_relinquish)
+{
+	/* Loop, giving memory back to the sender. */
+	for (;;) {
+		EXPECT_EQ(spci_msg_recv(SPCI_MSG_RECV_BLOCK), SPCI_SUCCESS);
+		uint8_t *ptr;
+
+		struct spci_message *recv_buf = SERVICE_RECV_BUFFER();
+		struct spci_message *send_buf = SERVICE_SEND_BUFFER();
+		struct spci_memory_region *memory_region =
+			(struct spci_memory_region *)(spci_get_lend_descriptor(
+							      recv_buf)
+							      ->payload);
+
+		ptr = (uint8_t *)memory_region->constituents[0].address;
+		/* Relevant information read, mailbox can be cleared. */
+		hf_mailbox_clear();
+
+		/* Check that one has access to the shared region. */
+		for (int i = 0; i < PAGE_SIZE; ++i) {
+			ptr[i]++;
+		}
+
+		hf_mailbox_clear();
+		/* Give the memory back and notify the sender. */
+		spci_memory_relinquish(
+			send_buf, HF_PRIMARY_VM_ID, recv_buf->target_vm_id,
+			memory_region->constituents, memory_region->count, 0);
+		spci_msg_send(0);
+
+		/*
+		 * Try and access the memory which will cause a fault unless the
+		 * memory has been shared back again.
+		 */
+		ptr[0] = 123;
+	}
+}
+
+/**
+ * SPCI: Ensure that we can't relinquish donated memory.
+ */
+TEST_SERVICE(spci_memory_donate_relinquish)
+{
+	for (;;) {
+		EXPECT_EQ(spci_msg_recv(SPCI_MSG_RECV_BLOCK), SPCI_SUCCESS);
+		uint8_t *ptr;
+
+		struct spci_message *recv_buf = SERVICE_RECV_BUFFER();
+		struct spci_message *send_buf = SERVICE_SEND_BUFFER();
+		struct spci_memory_region *memory_region =
+			spci_get_donated_memory_region(recv_buf);
+		hf_mailbox_clear();
+
+		ptr = (uint8_t *)memory_region->constituents[0].address;
+
+		/* Check that one has access to the shared region. */
+		for (int i = 0; i < PAGE_SIZE; ++i) {
+			ptr[i]++;
+		}
+		/* Give the memory back and notify the sender. */
+		spci_memory_relinquish(
+			send_buf, HF_PRIMARY_VM_ID, recv_buf->target_vm_id,
+			memory_region->constituents, memory_region->count, 0);
+		EXPECT_EQ(spci_msg_send(0), SPCI_INVALID_PARAMETERS);
+
+		/* Ensure we still have access to the memory. */
+		ptr[0] = 123;
+
+		spci_yield();
+	}
+}
+
+/**
+ * SPCI: Receive memory and attempt to donate from primary VM.
+ */
+TEST_SERVICE(spci_lend_invalid_source)
+{
+	EXPECT_EQ(spci_msg_recv(SPCI_MSG_RECV_BLOCK), SPCI_SUCCESS);
+
+	struct spci_message *recv_buf = SERVICE_RECV_BUFFER();
+	struct spci_message *send_buf = SERVICE_SEND_BUFFER();
+	struct spci_memory_region *memory_region =
+		(struct spci_memory_region *)(spci_get_lend_descriptor(recv_buf)
+						      ->payload);
+	hf_mailbox_clear();
+
+	/* Attempt to relinquish from primary VM. */
+	spci_memory_relinquish(send_buf, recv_buf->target_vm_id,
+			       HF_PRIMARY_VM_ID, memory_region->constituents,
+			       memory_region->count, 0);
+	EXPECT_EQ(spci_msg_send(0), SPCI_INVALID_PARAMETERS);
+
+	/* Give the memory back and notify the sender. */
+	spci_memory_relinquish(
+		send_buf, HF_PRIMARY_VM_ID, recv_buf->target_vm_id,
+		memory_region->constituents, memory_region->count, 0);
+	EXPECT_EQ(spci_msg_send(0), SPCI_SUCCESS);
+
+	/* Ensure we cannot lend from the primary to another secondary. */
+	spci_memory_lend(send_buf, SERVICE_VM1, HF_PRIMARY_VM_ID,
+			 memory_region->constituents, memory_region->count, 0,
+			 SPCI_LEND_RW_X, SPCI_LEND_NORMAL_MEM,
+			 SPCI_LEND_CACHE_WRITE_BACK, SPCI_LEND_OUTER_SHAREABLE);
+	EXPECT_EQ(spci_msg_send(0), SPCI_INVALID_PARAMETERS);
+	spci_yield();
+}
+
+/**
+ * SPCI: Attempt to execute an instruction from the lent memory.
+ */
+TEST_SERVICE(spci_memory_lend_relinquish_X)
+{
+	for (;;) {
+		EXPECT_EQ(spci_msg_recv(SPCI_MSG_RECV_BLOCK), SPCI_SUCCESS);
+		uint64_t *ptr;
+
+		struct spci_message *recv_buf = SERVICE_RECV_BUFFER();
+		struct spci_message *send_buf = SERVICE_SEND_BUFFER();
+		struct spci_memory_region *memory_region =
+			(struct spci_memory_region *)(spci_get_lend_descriptor(
+							      recv_buf)
+							      ->payload);
+		hf_mailbox_clear();
+
+		ptr = (uint64_t *)memory_region->constituents[0].address;
+		/*
+		 * Verify that the instruction in memory is the encoded RET
+		 * instruction.
+		 */
+		EXPECT_EQ(*ptr, 0xD65F03C0);
+		/* Try to execute instruction from the shared memory region. */
+		__asm__ volatile("blr %0" ::"r"(ptr));
+
+		/* Release the memory again. */
+		spci_memory_relinquish(
+			send_buf, HF_PRIMARY_VM_ID, recv_buf->target_vm_id,
+			memory_region->constituents, memory_region->count, 0);
+		EXPECT_EQ(spci_msg_send(0), SPCI_SUCCESS);
+	}
+}
+
+/**
+ * SPCI: Attempt to read and write to a shared page.
+ */
+TEST_SERVICE(spci_memory_lend_relinquish_RW)
+{
+	for (;;) {
+		EXPECT_EQ(spci_msg_recv(SPCI_MSG_RECV_BLOCK), SPCI_SUCCESS);
+		uint8_t *ptr;
+
+		struct spci_message *recv_buf = SERVICE_RECV_BUFFER();
+		struct spci_message *send_buf = SERVICE_SEND_BUFFER();
+		struct spci_memory_region *memory_region =
+			(struct spci_memory_region *)(spci_get_lend_descriptor(
+							      recv_buf)
+							      ->payload);
+		hf_mailbox_clear();
+
+		ptr = (uint8_t *)memory_region->constituents[0].address;
+
+		/* Check that we have read access. */
+		for (int i = 0; i < PAGE_SIZE; ++i) {
+			EXPECT_EQ(ptr[i], 'b');
+		}
+
+		/* Return control to primary, to verify shared access. */
+		spci_yield();
+
+		/* Attempt to modify the memory. */
+		for (int i = 0; i < PAGE_SIZE; ++i) {
+			ptr[i]++;
+		}
+
+		spci_memory_relinquish(
+			send_buf, HF_PRIMARY_VM_ID, recv_buf->target_vm_id,
+			memory_region->constituents, memory_region->count, 0);
+		EXPECT_EQ(spci_msg_send(0), SPCI_SUCCESS);
+	}
+}
+
+/**
+ * SPCI: Attempt to modify below the lower bound for the lent memory.
+ */
+TEST_SERVICE(spci_lend_check_lower_bound)
+{
+	EXPECT_EQ(spci_msg_recv(SPCI_MSG_RECV_BLOCK), SPCI_SUCCESS);
+	uint8_t *ptr;
+
+	struct spci_message *recv_buf = SERVICE_RECV_BUFFER();
+	struct spci_memory_region *memory_region =
+		(struct spci_memory_region *)(spci_get_lend_descriptor(recv_buf)
+						      ->payload);
+	hf_mailbox_clear();
+
+	ptr = (uint8_t *)memory_region->constituents[0].address;
+
+	/* Check that one cannot access before donated region. */
+	ptr[-1]++;
+}
+
+/**
+ * SPCI: Attempt to modify above the upper bound for the lent memory.
+ */
+TEST_SERVICE(spci_lend_check_upper_bound)
+{
+	EXPECT_EQ(spci_msg_recv(SPCI_MSG_RECV_BLOCK), SPCI_SUCCESS);
+	uint8_t *ptr;
+
+	struct spci_message *recv_buf = SERVICE_RECV_BUFFER();
+	struct spci_memory_region *memory_region =
+		(struct spci_memory_region *)(spci_get_lend_descriptor(recv_buf)
+						      ->payload);
+	hf_mailbox_clear();
+
+	ptr = (uint8_t *)memory_region->constituents[0].address;
+
+	/* Check that one cannot access after donated region. */
+	ptr[PAGE_SIZE]++;
+}
+
+TEST_SERVICE(spci_memory_lend_twice)
+{
+	EXPECT_EQ(spci_msg_recv(SPCI_MSG_RECV_BLOCK), SPCI_SUCCESS);
+	uint8_t *ptr;
+
+	struct spci_message *recv_buf = SERVICE_RECV_BUFFER();
+	struct spci_message *send_buf = SERVICE_SEND_BUFFER();
+	struct spci_memory_region *memory_region =
+		(struct spci_memory_region *)(spci_get_lend_descriptor(recv_buf)
+						      ->payload);
+	hf_mailbox_clear();
+
+	ptr = (uint8_t *)memory_region->constituents[0].address;
+
+	/* Check that we have read access. */
+	for (int i = 0; i < PAGE_SIZE; ++i) {
+		EXPECT_EQ(ptr[i], 'b');
+	}
+
+	/* Return control to primary. */
+	spci_yield();
+
+	/* Attempt to modify the memory. */
+	for (int i = 0; i < PAGE_SIZE; ++i) {
+		ptr[i]++;
+	}
+
+	for (int i = 1; i < PAGE_SIZE * 2; i++) {
+		memory_region->constituents[0].address = (uint64_t)ptr + i;
+
+		/* Fail to lend the memory back to the primary. */
+		spci_memory_lend(
+			send_buf, SERVICE_VM1, HF_PRIMARY_VM_ID,
+			memory_region->constituents, memory_region->count, 0,
+			SPCI_LEND_RW_X, SPCI_LEND_NORMAL_MEM,
+			SPCI_LEND_CACHE_WRITE_BACK, SPCI_LEND_OUTER_SHAREABLE);
+		EXPECT_EQ(spci_msg_send(0), SPCI_INVALID_PARAMETERS);
+	}
+
+	spci_memory_relinquish(
+		send_buf, HF_PRIMARY_VM_ID, recv_buf->target_vm_id,
+		memory_region->constituents, memory_region->count, 0);
+	EXPECT_EQ(spci_msg_send(0), SPCI_SUCCESS);
 }
