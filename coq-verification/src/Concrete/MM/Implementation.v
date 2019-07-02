904f58df7ac92e49fa9f8d557c3d4630ea4a39b9
Jade Philipoom <jadep@google.com>
2019-07-02 11:38:04 +0100

    move stuff from mm.c to MM/Implementation.v

---
+++ coq-verification/src/Concrete/MM/Implementation.v
@@ -0,0 +1,83 @@
+Require Import Coq.NArith.BinNat.
+Require Import Hafnium.Concrete.Datatypes.
+Require Import Hafnium.Concrete.Notations.
+Require Import Hafnium.Concrete.State.
+Require Import Hafnium.Concrete.Assumptions.Addr.
+Require Import Hafnium.Concrete.Assumptions.Constants.
+Require Import Hafnium.Concrete.Assumptions.Datatypes.
+Require Import Hafnium.Concrete.Assumptions.Mpool.
+
+(*** This file transcribes necessary functions from mm.c, with the original C in
+     comments alongside. ***)
+
+(* typedef uintvaddr_t ptable_addr_t; *)
+Definition ptable_addr_t : Type := uintvaddr_t.
+Bind Scope N_scope with ptable_addr_t.
+
+(* static ptable_addr_t mm_round_down_to_page(ptable_addr_t addr) *)
+Definition mm_round_down_to_page (addr : ptable_addr_t) : ptable_addr_t :=
+  (* return addr & ~((ptable_addr_t)(PAGE_SIZE - 1)); *)
+   N.land addr (N.lnot (PAGE_SIZE - 1) (N.size addr)).
+
+(* static ptable_addr_t mm_round_up_to_page(ptable_addr_t addr) *)
+Definition mm_round_up_to_page (addr : ptable_addr_t) : ptable_addr_t :=
+  (* return mm_round_down_to_page(addr + PAGE_SIZE - 1); *)
+  mm_round_down_to_page (addr + PAGE_SIZE - 1).
+
+(*
+  /**
+  * Gets the attributes applies to the given range of addresses in the stage-2
+  * table.
+  *
+  * The value returned in `attrs` is only valid if the function returns true.
+  *
+  * Returns true if the whole range has the same attributes and false otherwise.
+  */
+  static bool mm_vm_get_attrs(struct mm_ptable *t, ptable_addr_t begin,
+                              ptable_addr_t end, uint64_t *attrs)
+ *)
+(* N.B. instead of passing in a page table we pass in the vm whose root table
+   we are searching *)
+Definition mm_vm_get_attrs
+           (s : concrete_state)
+           (t : ptable_pointer)
+           (begin end_ : ptable_addr_t) : bool * attributes :=
+  (false, 0%N). (* TODO *)
+(*
+  /**
+  * Gets the mode of the give range of intermediate physical addresses if they
+  * are mapped with the same mode.
+  *
+  * Returns true if the range is mapped with the same mode and false otherwise.
+  */
+  bool mm_vm_get_mode(struct mm_ptable *t, ipaddr_t begin, ipaddr_t end,
+       int *mode)
+ *)
+(* N.B. the comment above the function means "the entire range of addresses
+   has one consistent mode" and not "the range of addresses has the same
+   mode as is indicated by the pointer passed in". *)
+Definition mm_vm_get_mode
+           (s : concrete_state)
+           (t : ptable_pointer)
+           (begin end_ : ipaddr_t) : bool * mode_t :=
+  (false, 0%N). (* TODO *)
+
+(*
+  /**
+  * Updates a VM's page table such that the given physical address range is
+  * mapped in the address space at the corresponding address range in the
+  * architecture-agnostic mode provided.
+  */
+  bool mm_vm_identity_map(struct mm_ptable *t, paddr_t begin, paddr_t end,
+                          int mode, ipaddr_t *ipa, struct mpool *ppool)
+ *)
+(* N.B. for now, ignoring the ipa argument, which is set to NULL in all calls
+   I've found so far. *)
+Definition mm_vm_identity_map
+           (s : concrete_state)
+           (t : ptable_pointer)
+           (begin : paddr_t)
+           (end_ : paddr_t)
+           (mode : mode_t)
+           (ppool : mpool) : (bool * concrete_state * mpool) :=
+  (false, s, ppool).
