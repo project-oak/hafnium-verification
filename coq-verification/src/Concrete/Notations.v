e0250d3e0ed7e55ee28ca317cacf3f150c598e11
Jade Philipoom <jadep@google.com>
2019-07-02 11:06:46 +0100

    put code-readability boilerplate into its own file

---
+++ coq-verification/src/Concrete/Notations.v
@@ -0,0 +1,24 @@
+Require Import Coq.NArith.BinNat.
+Require Import Hafnium.Concrete.Datatypes.
+
+(*** This file is mostly boilerplate to coerce Coq's notation system into making
+     the model more readable ***)
+
+(* nicer display of mm_mode operations *)
+Definition mm_mode_set_flag (m : int) (flag : nat) : int :=
+  N.setbit m (N.of_nat flag).
+Definition mm_mode_get_flag (m : int) (flag : nat) : bool :=
+  N.testbit m (N.of_nat flag).
+Notation "[ x | .. | y ]" :=
+  (mm_mode_set_flag .. (mm_mode_set_flag 0 x) .. y)
+    (at level 49, y at level 0, only parsing) : N_scope.
+Notation "x & y " :=
+  (mm_mode_get_flag x y)
+    (at level 49, y at level 0, only parsing) : N_scope.
+
+Bind Scope N_scope with mode_t.
+Bind Scope N_scope with attributes.
+Bind Scope N_scope with uintpaddr_t.
+Bind Scope N_scope with uintvaddr_t.
+
+Notation "! x" := (negb x) (at level 200) : bool_scope.
