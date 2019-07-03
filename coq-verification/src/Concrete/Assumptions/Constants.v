6dbee464fc93691b8485cbcd9b6e5510c56e96ae
Jade Philipoom <jadep@google.com>
2019-07-03 11:59:41 +0100

    add some more functions from header files

--- coq-verification/src/Concrete/Assumptions/Constants.v
+++ coq-verification/src/Concrete/Assumptions/Constants.v
@@ -5,8 +5,8 @@ Import ListNotations.
      in e.g. mm.h. These are left abstract because none of the proofs should
      depend on their exact values. ***)
 
-(* PAGE_SIZE is a natural number *)
-Axiom PAGE_SIZE : nat.
+(* page size parameters are natural numbers *)
+Axiom PAGE_SIZE PAGE_BITS PAGE_LEVEL_BITS : nat.
 
 (* PTEs per page -- natural number *)
 Axiom MM_PTE_PER_PAGE : nat.
@@ -22,3 +22,9 @@ Axiom MM_MODE_R MM_MODE_W MM_MODE_X MM_MODE_INVALID MM_MODE_UNOWNED
 Axiom mm_modes_distinct :
   NoDup [MM_MODE_R; MM_MODE_W; MM_MODE_X; MM_MODE_INVALID; MM_MODE_UNOWNED;
            MM_MODE_SHARED].
+
+(* mm flags are all natural numbers*)
+(* N.B. : In the same way as mm_mode flags, these are not binary numbers with
+   a single bit set but rather represent the index of the bit that would be set,
+   so that the single-bit-set format of the number is obvious by construction. *)
+Axiom MM_FLAG_STAGE1 : nat.
