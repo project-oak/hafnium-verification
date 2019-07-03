f4e5ef6fbee36e8295e696eef1865fc908873cc1
Jade Philipoom <jadep@google.com>
2019-07-03 11:59:41 +0100

    add some more functions from header files

--- coq-verification/src/Concrete/Assumptions/Addr.v
+++ coq-verification/src/Concrete/Assumptions/Addr.v
@@ -1,4 +1,5 @@
 Require Import Hafnium.Concrete.Datatypes.
+Require Import Hafnium.Concrete.Assumptions.Datatypes.
 
 (*** This file defines (the necessary parts of) the API provided by addr.h,
      which is assumed correct for now. In order to replace this assumption with
@@ -9,6 +10,8 @@ Axiom ipaddr_t : Type.
 
 Axiom paddr_t : Type.
 
+Axiom vaddr_t : Type.
+
 Axiom pa_addr : paddr_t -> uintpaddr_t.
 
 Axiom pa_difference : paddr_t -> paddr_t -> size_t.
@@ -17,7 +20,11 @@ Axiom ipa_addr : ipaddr_t -> uintpaddr_t.
 
 Axiom ipa_add : ipaddr_t -> size_t -> ipaddr_t.
 
+Axiom va_from_pa : paddr_t -> vaddr_t.
+
 Axiom pa_from_ipa : ipaddr_t -> paddr_t.
+
+Axiom ptr_from_va : vaddr_t -> ptable_pointer.
 
 Axiom is_aligned : uintpaddr_t -> nat (* PAGE_SIZE *) -> bool.
 
