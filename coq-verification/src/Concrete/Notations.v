b4782d300c25449b419317d62169dd45962de2f6
Jade Philipoom <jadep@google.com>
2019-07-05 11:27:19 +0100

    make bit-testing more readable

--- coq-verification/src/Concrete/Notations.v
+++ coq-verification/src/Concrete/Notations.v
@@ -17,6 +17,7 @@ Bind Scope N_scope with uintpaddr_t.
 Bind Scope N_scope with uintvaddr_t.
 
 Notation "! x" := (negb x) (at level 200) : bool_scope.
+Notation "x != y" := (negb (N.eqb x y)) (at level 199) : bool_scope.
 
 Coercion N.of_nat : nat >-> N. (* change nat to N automatically *)
 Set Printing Coercions. (* when printing, show N.of_nat explicitly *)
