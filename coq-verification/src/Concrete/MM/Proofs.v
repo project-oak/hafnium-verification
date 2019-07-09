7fe0f2c9be7546c1ab1412fec8e12d34fc99c2e0
Jade Philipoom <jadep@google.com>
2019-07-09 16:06:47 +0100

    make proof stubs for the mm functions called in api_clear_memory

---
+++ coq-verification/src/Concrete/MM/Proofs.v
@@ -0,0 +1,38 @@
+Require Import Coq.Arith.PeanoNat.
+Require Import Hafnium.AbstractModel.
+Require Import Hafnium.Concrete.State.
+Require Import Hafnium.Concrete.Datatypes.
+Require Import Hafnium.Util.Tactics.
+Require Import Hafnium.Concrete.Assumptions.Addr.
+Require Import Hafnium.Concrete.Assumptions.Mpool.
+Require Import Hafnium.Concrete.MM.Implementation.
+
+(*** This file contains correctness proofs for the functions in mm.c, as
+     transcribed in MM/Implementation.v ***)
+
+Lemma mm_identity_map_represents
+      {ap : abstract_state_parameters} {cp : concrete_params}
+      (conc : concrete_state)
+      begin end_ mode ppool :
+  (exists abst, represents abst conc) ->
+  let conc' := snd (fst (mm_identity_map conc begin end_ mode ppool)) in
+  exists abst', represents abst' conc'.
+Admitted.
+
+Lemma mm_defrag_represents
+      {ap : abstract_state_parameters} {cp : concrete_params}
+      (conc : concrete_state)
+      ppool :
+  (exists abst, represents abst conc) ->
+  let conc' := fst (mm_defrag conc ppool) in
+  exists abst', represents abst' conc'.
+Admitted.
+
+Lemma mm_unmap_represents
+      {ap : abstract_state_parameters} {cp : concrete_params}
+      (conc : concrete_state)
+      begin end_ ppool :
+  (exists abst, represents abst conc) ->
+  let conc' := snd (fst (mm_unmap conc begin end_ ppool)) in
+  exists abst', represents abst' conc'.
+Admitted.
