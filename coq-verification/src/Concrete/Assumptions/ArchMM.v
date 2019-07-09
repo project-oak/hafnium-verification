882314e709efb1313c82d5cf91fc819771369ae5
Jade Philipoom <jadep@google.com>
2019-07-09 10:23:02 +0100

    transcribe mm_identity_map

--- coq-verification/src/Concrete/Assumptions/ArchMM.v
+++ coq-verification/src/Concrete/Assumptions/ArchMM.v
@@ -44,6 +44,8 @@ Axiom arch_mm_stage2_root_table_count : nat.
 
 Axiom arch_mm_stage1_root_table_count : nat.
 
+Axiom arch_mm_mode_to_stage1_attrs : mode_t -> attributes.
+
 Axiom arch_mm_mode_to_stage2_attrs : mode_t -> attributes.
 
 Axiom arch_mm_clear_pa : paddr_t -> paddr_t.
