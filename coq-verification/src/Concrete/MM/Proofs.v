f7892157829e63d26bf1a4b1239e1f39ee0414fc
Jade Philipoom <jadep@google.com>
2019-07-09 16:47:38 +0100

    preemptively add proof stubs for the dependencies of mm_identity_map

--- coq-verification/src/Concrete/MM/Proofs.v
+++ coq-verification/src/Concrete/MM/Proofs.v
@@ -1,10 +1,12 @@
 Require Import Coq.Arith.PeanoNat.
+Require Import Coq.Lists.List.
 Require Import Hafnium.AbstractModel.
 Require Import Hafnium.Concrete.State.
 Require Import Hafnium.Concrete.Datatypes.
 Require Import Hafnium.Util.Tactics.
 Require Import Hafnium.Concrete.Assumptions.Addr.
 Require Import Hafnium.Concrete.Assumptions.Mpool.
+Require Import Hafnium.Concrete.MM.Datatypes.
 Require Import Hafnium.Concrete.MM.Implementation.
 
 (*** This file contains correctness proofs for the functions in mm.c, as
@@ -16,10 +18,62 @@ Section Proofs.
 
   Local Definition preserves_represents
         (f : concrete_state -> concrete_state) : Prop :=
-    forall (ap : abstract_state_parameters) (cp : concrete_params)
-           (conc : concrete_state),
+    forall (conc : concrete_state),
       (exists abst, represents abst conc) ->
       exists abst', represents abst' (f conc).
+  Hint Unfold preserves_represents.
+
+  Lemma mm_free_page_pte_represents
+        (conc : concrete_state)
+        pte level ppool :
+    preserves_represents
+      (fun conc => fst (mm_free_page_pte conc pte level ppool)).
+  Admitted.
+
+  Lemma mm_replace_entry_represents
+        (conc : concrete_state)
+        begin t pte_index new_pte level flags ppool :
+    preserves_represents
+      (fun conc =>
+         snd (fst (mm_replace_entry
+                     conc begin t pte_index new_pte level flags ppool))).
+  Admitted.
+
+  Lemma mm_populate_table_pte_represents
+        (conc : concrete_state)
+        begin t pte_index level flags ppool :
+    preserves_represents
+      (fun conc =>
+         snd (fst (mm_populate_table_pte
+                     conc begin t pte_index level flags ppool))).
+  Admitted.
+
+  Lemma mm_map_level_represents
+        (conc : concrete_state)
+        t begin end_ attrs table level flags ppool :
+    preserves_represents
+      (fun conc =>
+         snd (fst (mm_map_level
+                     conc t begin end_ attrs table level flags ppool))).
+  Admitted.
+
+  Lemma mm_map_root_represents
+        (conc : concrete_state)
+        t begin end_ attrs root_level flags ppool :
+    preserves_represents
+      (fun conc =>
+         snd (fst (mm_map_root
+                     conc t begin end_ attrs root_level flags ppool))).
+  Admitted.
+
+  Lemma mm_ptable_identity_update_represents
+        (conc : concrete_state)
+        t pa_begin pa_end attrs flags ppool :
+    preserves_represents
+      (fun conc =>
+         snd (fst (mm_ptable_identity_update
+                     conc t pa_begin pa_end attrs flags ppool))).
+  Admitted.
 
   Lemma mm_identity_map_represents
         (conc : concrete_state)
