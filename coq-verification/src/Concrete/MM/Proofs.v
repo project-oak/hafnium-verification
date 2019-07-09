94e0a10fbf2661fae253a3cc17bb5c4ab946875d
Jade Philipoom <jadep@google.com>
2019-07-09 16:31:36 +0100

    indent (no code changes)

--- coq-verification/src/Concrete/MM/Proofs.v
+++ coq-verification/src/Concrete/MM/Proofs.v
@@ -14,27 +14,27 @@ Section Proofs.
   Context {ap : @abstract_state_parameters paddr_t nat}
           {cp : concrete_params}.
 
-Lemma mm_identity_map_represents
-      (conc : concrete_state)
-      begin end_ mode ppool :
-  (exists abst, represents abst conc) ->
-  let conc' := snd (fst (mm_identity_map conc begin end_ mode ppool)) in
-  exists abst', represents abst' conc'.
-Admitted.
+  Lemma mm_identity_map_represents
+        (conc : concrete_state)
+        begin end_ mode ppool :
+    (exists abst, represents abst conc) ->
+    let conc' := snd (fst (mm_identity_map conc begin end_ mode ppool)) in
+    exists abst', represents abst' conc'.
+  Admitted.
 
-Lemma mm_defrag_represents
-      (conc : concrete_state)
-      ppool :
-  (exists abst, represents abst conc) ->
-  let conc' := fst (mm_defrag conc ppool) in
-  exists abst', represents abst' conc'.
-Admitted.
+  Lemma mm_defrag_represents
+        (conc : concrete_state)
+        ppool :
+    (exists abst, represents abst conc) ->
+    let conc' := fst (mm_defrag conc ppool) in
+    exists abst', represents abst' conc'.
+  Admitted.
 
-Lemma mm_unmap_represents
-      (conc : concrete_state)
-      begin end_ ppool :
-  (exists abst, represents abst conc) ->
-  let conc' := snd (fst (mm_unmap conc begin end_ ppool)) in
-  exists abst', represents abst' conc'.
-Admitted.
+  Lemma mm_unmap_represents
+        (conc : concrete_state)
+        begin end_ ppool :
+    (exists abst, represents abst conc) ->
+    let conc' := snd (fst (mm_unmap conc begin end_ ppool)) in
+    exists abst', represents abst' conc'.
+  Admitted.
 End Proofs.
