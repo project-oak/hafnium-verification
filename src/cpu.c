9d0ab7c72ad3ca1b892274c03de40fb7518da0ed
Park, Sanguk <noorim.ailen@gmail.com>
2019-07-26 15:32:01 +0900

    Test cpu_init.

--- src/cpu.c
+++ src/cpu.c
@@ -1,9 +1,90 @@
+/*
+ * Copyright 2018 The Hafnium Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "hf/cpu.h"
-#include "hf/mm.h"
 
-extern char callstacks[MAX_CPUS][PAGE_SIZE];
+#include <stdalign.h>
 
-struct cpu cpus = {
-    .is_on = 1,
-    .stack_bottom = &callstacks[0][PAGE_SIZE],
-}; 
+#include "hf/arch/cpu.h"
+
+#include "hf/api.h"
+#include "hf/check.h"
+#include "hf/dlog.h"
+#include "hf/spci.h"
+#include "hf/std.h"
+#include "hf/vm.h"
+
+#include "vmapi/hf/call.h"
+
+#define STACK_SIZE PAGE_SIZE
+
+/* The stack to be used by the CPUs. */
+extern char callstacks[MAX_CPUS][STACK_SIZE];
+
+/* State of all supported CPUs. The stack of the first one is initialized. */
+struct cpu cpus[MAX_CPUS] = {
+	{
+		.is_on = 1,
+		.stack_bottom = &callstacks[0][STACK_SIZE],
+	},
+};
+
+uint32_t cpu_count = 1;
+
+static void cpu_init(struct cpu *c)
+{
+	/* TODO: Assumes that c is zeroed out already. */
+	sl_init(&c->lock);
+}
+
+void cpu_module_init(const cpu_id_t *cpu_ids, size_t count)
+{
+	uint32_t i;
+	uint32_t j;
+	cpu_id_t boot_cpu_id = cpus[0].id;
+	bool found_boot_cpu = false;
+
+	cpu_count = count;
+
+	/*
+	 * Initialize CPUs with the IDs from the configuration passed in. The
+	 * CPUs after the boot CPU are initialized in reverse order. The boot
+	 * CPU is initialized when it is found or in place of the last CPU if it
+	 * is not found.
+	 */
+	j = cpu_count;
+	for (i = 0; i < cpu_count; ++i) {
+		struct cpu *c;
+		cpu_id_t id = cpu_ids[i];
+
+		if (found_boot_cpu || id != boot_cpu_id) {
+			c = &cpus[--j];
+		} else {
+			found_boot_cpu = true;
+			c = &cpus[0];
+		}
+
+		cpu_init(c);
+		c->id = id;
+		c->stack_bottom = &callstacks[i][STACK_SIZE];
+	}
+
+	if (!found_boot_cpu) {
+		/* Boot CPU was initialized but with wrong ID. */
+		dlog("Boot CPU's ID not found in config.");
+		cpus[0].id = boot_cpu_id;
+	}
+}
